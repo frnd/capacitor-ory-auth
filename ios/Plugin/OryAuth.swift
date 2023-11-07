b007bf8c3f05dc648bca7a28d5045eeb07f02cc9
Fernando González <fernando@tuta.io>
2023-11-07 11:53:13 +0100

    ios WIP.

--- ios/Plugin/OryAuth.swift
+++ ios/Plugin/OryAuth.swift
@@ -1,8 +1,24 @@
 import Foundation
 
 @objc public class OryAuth: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func session() -> Bool {
+        return false
     }
+    
+    @objc public func login(identifier: String, password: String) -> String {
+        return ""
+    }
+    
+    @objc public func logout() -> String {
+        return ""
+    }
+    
+    @objc public func signin(identifier: String, traits: String) -> String {
+        return ""
+    }
+    
+    @objc public func verify(id: String, code: String, email: String) -> String {
+        return ""
+    }
+    
 }
--- ios/Plugin/OryAuthPlugin.swift
+++ ios/Plugin/OryAuthPlugin.swift
@@ -7,36 +7,61 @@ import Capacitor
  */
 @objc(OryAuthPlugin)
 public class OryAuthPlugin: CAPPlugin {
-    private let implementation = OryAuth()
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
-    }
+    private var auth: OryAuth? = nil
 
     @objc func initialize(_ call: CAPPluginCall) {
-        print("initializeeee")
+        print("initialize")
+        let basePath = call.getString("basePath") ?? ""
+        OpenAPIClientAPI.basePath = basePath
+        auth = OryAuth()
         call.resolve()
     }
     @objc func session(_ call: CAPPluginCall) {
-        print("session")
-        call.resolve()
+        
+        if(auth == nil) {
+            call.reject("Initialize plugin before use.");
+        } else {
+            call.resolve([
+                "active": auth?.session() ?? false
+            ])
+        }
+        
+        
     }
     @objc func signIn(_ call: CAPPluginCall) {
+        
+        if(auth == nil) {
+            call.reject("Initialize plugin before use.");
+        }
+        
         print("signIn")
         call.resolve()
     }
     @objc func verify(_ call: CAPPluginCall) {
+        
+        if(auth == nil) {
+            call.reject("Initialize plugin before use.");
+        }
+        
         print("verify")
         call.resolve()
     }
     @objc func logIn(_ call: CAPPluginCall) {
+        
+        if(auth == nil) {
+            call.reject("Initialize plugin before use.");
+        }
+        
         print("logIn")
         call.resolve()
     }
     @objc func logOut(_ call: CAPPluginCall) {
+        
+        if(auth == nil) {
+            call.reject("Initialize plugin before use.");
+        }
+        
         print("logOut")
         call.resolve()
     }
